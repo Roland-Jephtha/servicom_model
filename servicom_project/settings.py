0182876dcb5148c1cde37d4c0b4e00136cb7cf9d
Jephtha <jephtroland832@gmail.com>
2025-09-08 11:23:28 +0100

    All in one

--- servicom_project/settings.py
+++ servicom_project/settings.py
@@ -156,11 +156,10 @@ MESSAGE_TAGS = {
 
 
 
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'assignysite@gmail.com'
-EMAIL_HOST_PASSWORD = "zoeyyklwchzxdgbj"
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_USER = 'ekongmary2019@gmail.com'
+EMAIL_HOST_PASSWORD = "vjcdthxgijstarvc"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
